b8ea1bdf72c0ce65959d2f8e4e37d4bfe2e375c0
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-06-30 11:28:03 -0300

    Coloca participante e lista

--- src/app.js
+++ src/app.js
@@ -1,21 +1,19 @@
-/// Imports API Connection & Security
+/// Imports Bibliotecas ///
+import dayjs from "dayjs";
+
+/// Imports API Connection & Security ///
 import express from "express";
 import cors from "cors";
 
-
-/// Imports Database & Security
+/// Imports Database & Security ///
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
-
 
 /// API Connection ///
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-
-
 
 /// Database Connection ///
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
@@ -25,8 +23,31 @@ mongoClient.connect()
     .then(() => db = mongoClient.db())
     .catch((err) => console.log(err.message))
 
+/// Código ///
 
 
+
+// Inserindo Usuário //
+app.post("/participants", (req, res) => {
+    const nome = req.body.name;
+
+    db.collection("participants").insertOne({ name: nome, lastStatus: Date.now() });
+    const prom = db.collection("messages").insertOne({
+        from: nome,
+        to: 'Todos', text: 'entra na sala...',
+        type: 'status',
+        time: dayjs().format('HH:mm:ss')
+    })
+    prom.then(() => res.sendStatus(201))
+})
+
+
+// Achando os Usuários //
+
+app.get("/participants", (req,res) => {
+    const users = db.collection("participantes").find();
+    res.send(users);
+})
 
 
 
