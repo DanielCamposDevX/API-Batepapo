438454bce1b52c46c9f5253e4d312ed842d4c15f
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-07-01 08:41:05 -0300

    Async functions update

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-/// Imports Bibliotecas ///
+/// Package imports ///
 import dayjs from "dayjs";
+import Joi from "joi";
 
 /// Imports API Connection & Security ///
 import express from "express";
@@ -23,32 +24,75 @@ mongoClient.connect()
     .then(() => db = mongoClient.db())
     .catch((err) => console.log(err.message))
 
-/// Código ///
 
 
+///////////////////// Code ///////////////////////
 
-// Inserindo Usuário //
-app.post("/participants", (req, res) => {
-    const nome = req.body.name;
-
-    db.collection("participants").insertOne({ name: nome, lastStatus: Date.now() });
-    const prom = db.collection("messages").insertOne({
-        from: nome,
-        to: 'Todos', text: 'entra na sala...',
-        type: 'status',
-        time: dayjs().format('HH:mm:ss')
-    })
-    prom.then(() => res.sendStatus(201))
+//Schema for data Validation
+const schemauser = Joi.object({
+    name: Joi.string()
+        .min(1),
 })
 
 
-// Achando os Usuários //
+// User login //
+app.post("/participants", async (req, res) => {
 
-app.get("/participants", (req,res) => {
-    const users = db.collection("participantes").find();
-    res.send(users);
+    const data = req.body;
+    const validate = schemauser.validate(data, { abortEarly: false });
+
+    if (validate.error) {
+
+        //const errors = validate.error.details.map((detail) => detail.message);
+        return res.sendStatus(422)//.send(errors);
+
+    }
+    else {
+
+        const nome = data.name;
+
+        try {
+            const user = await db.collection("participants").findOne({ name: nome });
+
+            if (user) {
+                res.sendStatus(409)
+            }
+
+            else {
+
+                await db.collection("participants").insertOne({ name: nome, lastStatus: Date.now() });
+                await db.collection("messages").insertOne({
+                    from: nome,
+                    to: 'Todos', text: 'entra na sala...',
+                    type: 'status',
+                    time: dayjs().format('HH:mm:ss')
+                })
+                return res.sendStatus(201);
+            }
+        }
+
+        catch (error) {
+            return res.sendStatus(500);
+        }
+    }
 })
-,
+
+
+
+
+// Loading Users //
+
+app.get("/participants", async (req, res) => {
+    try {
+        const users = await db.collection("participants").find().toArray();
+        return res.send(users);
+    }
+    catch(error){
+        console.log(error);
+        return res.sendStatus(500);
+    }
+})
+
 
 
 
